3fd4bbad08b03bae1d1d423021429b232c863cf5
Lam Nguyen <lamcnguyen89@gmail.com>
2020-06-16 19:24:55 -0400

    Added pseudocode comments to structure the code code. Added some HTML elements

---
+++ quiz.js
@@ -0,0 +1,107 @@
+// Create questions as an Object
+// An object in array form is the best way because when you call an index number, you can select the group of relevant data efficiently. 
+//
+var questions = [
+    {
+        title: "Example Question 1:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    },
+    {
+        title: "Example Question 2:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    },
+    {
+        title: "Example Question 2:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    },
+    {
+        title: "Example Question 2:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    },
+];
+
+// Hook elements from the page (i.e var exampleEl = document.querySelector(".elementClass"))
+// Hook container element
+var containerEl = document.querySelector("")
+
+// Hook timer element
+
+// Create dynamic elements (i.e exampleDynamicEl = document.createElement("button"))
+// Create <h1> to  show Starting Heading
+// Create Button to Start Quiz
+// Create p tag to display the question
+
+// Declare global variables
+// Declare variable to store timer number
+// Declare variable to store current index
+
+// Required Functions*****/
+// function that loads content when the page first loads
+// function that shows the question and starts the timer
+// function that handles the timer
+// function that handles and displays the next question
+// function to check the answer and display the next question
+
+
+// function that loads content when page first loads:
+function openingPage() {
+    // Add text to Title Tag
+
+    // Add text to Button
+
+    // Append text to Container
+
+    // Append button to Container
+
+
+}
+
+// function that shows the question and starts the timer
+function startQuiz() {
+    // show timer function
+
+    // call next question function 
+
+
+}
+
+// function the handles the timer
+function showTimer() {
+    // display timer to screen
+
+    // create setInterval and store it to a variable
+
+    // inside setInterval and store it to a variable
+
+    // inside setInterval function:
+    // decrease timer by 1
+    // display timer on screen
+    // if timer goes down to 0, clear interval
+}
+
+// function that handles and displays the next question
+function nextQuestion() {
+    // Declare a variable to store current question. Assign the current question.
+
+    // Empty container element
+
+    // Add current question title to the question display variable
+
+    // Append the question display variable to the container
+
+    // Create a div element to wrap the "choices"
+
+    // for loop to:
+    // - create button elements for each choice
+    // - add a class to each button to be used with the event listener
+    // - add text to each button from question choices
+    // - append buttons to div element created to wrap choices
+
+    // append div element to container
+
+
+}
